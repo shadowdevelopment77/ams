d95e044fc972232aebf05ca173217be3284f13dc
shadowdevelopment77 <adlydevelopment37@gmail.com>
2026-05-29 20:00:58 +0700

    fix role contract

---
+++ backend/src/repositories/implementors/role.repository.ts
@@ -0,0 +1,41 @@
+import {UserRole, PrismaClient, User} from "../../../generated/prisma"
+import {IRoleRepository, UpdateRoleDTO, CreateRoleDTO} from "../interfaces/role.interface"
+
+export class RoleRepository implements IRoleRepository {
+  constructor(private prisma: PrismaClient) {}
+
+  
+
+  async findAll(): Promise<UserRole[]> {
+    return this.prisma.userRole.findMany({
+      where: { is_active: true, is_deleted: false },
+      orderBy: { id: "asc" }
+    })
+  }
+
+  async findById(id: number): Promise<UserRole | null> {
+    return this.prisma.userRole.findFirst({
+      where: { id, is_deleted: false }
+    })
+  }
+
+  async create(data: CreateRoleDTO): Promise<UserRole>{
+    return this.prisma.userRole.create({
+        data: {name: data.name},
+    })
+  }
+
+  async update(id: number, data: UpdateRoleDTO): Promise<UserRole>{
+    return this.prisma.userRole.update({
+        where: {id},
+        data: {name: data.name, is_active: data.is_active}
+    })
+  }
+
+  async softDelete(id:number): Promise<UserRole>{
+    return this.prisma.userRole.update({
+    where:{id, is_deleted: false},
+    data:{is_deleted: true, deleted_at: new Date(), is_active:false}
+})
+  }
+}
---
+++ backend/src/repositories/interfaces/role.interface.ts
@@ -0,0 +1,20 @@
+import {UserRole} from "../../../generated/prisma"
+
+
+export interface CreateRoleDTO{
+    name:string
+}
+
+
+export interface UpdateRoleDTO {
+  name?: string
+  is_active?: boolean
+}
+
+export interface IRoleRepository {
+  findAll(): Promise<UserRole[]>
+  findById(id: number): Promise<UserRole | null>
+  softDelete(id:number): Promise<UserRole>
+  create(data: CreateRoleDTO): Promise<UserRole>
+  update(id:number, data: UpdateRoleDTO): Promise<UserRole>
+}
