c2cadb2c264b4e66528ea718a0e860bdc4fc9b40
shadowdevelopment77 <adlydevelopment37@gmail.com>
2026-06-19 17:39:12 +0700

    add visit log repo

---
+++ backend/src/repositories/interfaces/visit-log.interface.ts
@@ -0,0 +1,24 @@
+import { VisitLog } from "../../../generated/prisma";
+import { BaseRepository, PaginatedResult, PaginationParams } from "./base.interface";
+
+export interface CreateVisitLogDTO {
+  user_id: string;
+  company_id: number;
+  photo_url: string;
+  date: Date;
+  latitude?: number;
+  longitude?: number;
+  location_addres?: string;
+  notes?: string;
+  visited_at?: Date;
+}
+
+export interface UpdateVisitLogDTO {
+  notes?: string;
+}
+
+
+export interface VisitLogRepository
+  extends BaseRepository<VisitLog, CreateVisitLogDTO, UpdateVisitLogDTO> {
+  findByUser(userId: string, date: Date, params?: PaginationParams): Promise<PaginatedResult<VisitLog>>;
+}
